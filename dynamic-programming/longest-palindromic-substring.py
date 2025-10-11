3947509a4daaad0173d0f4e33572a14d2ad1a6d7
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2025-10-11 01:26:25 -0400

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- dynamic-programming/longest-palindromic-substring.py
+++ dynamic-programming/longest-palindromic-substring.py
@@ -7,11 +7,11 @@ class Solution:
             l=r=mid
         if len(s)<2:
             return s
-        if len(s)==2:
-            if s[l]!=s[r]:
-                return s[l]
+            
         while l>0 and r< len(s)-1:
                 if s[l]==s[r]:
                     l=l-1
                     r=r+1
+                elif len[s]==2:
+                    return s[l]
         return s[l+1:r]
