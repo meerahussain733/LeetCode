bb4a2d444cfc50232e6287842dc8830fd5cd040f
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2025-10-11 01:23:56 -0400

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- two-pointers/longest-palindromic-substring.py
+++ two-pointers/longest-palindromic-substring.py
@@ -7,6 +7,9 @@ class Solution:
             l=r=mid
         if len(s)<2:
             return s
+        if len(s)==2:
+            if s[l]!=s[r]:
+                return s[l]
         while l>0 and r< len(s)-1:
                 if s[l]==s[r]:
                     l=l-1
