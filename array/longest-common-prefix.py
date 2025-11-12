0d883de6e958b67043a0b203c7ee8d31b1e6c8a2
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2025-11-11 23:54:43 -0500

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- array/longest-common-prefix.py
+++ array/longest-common-prefix.py
@@ -1,11 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix=strs[0]
-        for i, j in enumerate(prefix):
-            for string in strs:
-                if len(string) == i or string[i] != j:
-                    return prefix[:i]
-        return prefix
+        strs.sort()
+        if len(strs)==1:
+            return ""
+        k=""
+        first=strs[0]
+        last=strs[-1]
+        for i in range(len(first)):
+            if first[i]!=last[i]:
+                break
+            k+=first[i]
+        return k
         
 
         
