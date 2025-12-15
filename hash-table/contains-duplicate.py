d81c6900b81af013a30398aa72528ddd29662676
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2025-12-14 23:38:17 -0500

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

---
+++ hash-table/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen=[]
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True 
+            else:
+                seen.append(nums[i])
+        return False
+        
