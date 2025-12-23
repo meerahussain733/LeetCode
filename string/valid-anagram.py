e894fdd0c9ed96aea062fc387b6a8c0b35f8f306
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2025-12-23 00:21:16 -0500

    LC-valid-anagram [Runtime: 14 ms; Memory: 17.6 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -2,12 +2,22 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        s=sorted(s)
-        t=sorted(t)
-        for i in range(len(s)):
-            if s[i]!=t[i]:
-                return False
-        return True
+        freq_t={}
+        freq_s={}
+        for char in s:
+            if char in freq_s:
+                freq_s[char]+=1
+            else:
+                freq_s[char]=1
+        for char in t:
+            if char in freq_t:
+                freq_t[char]+=1
+            else:
+                freq_t[char]=1
+        if freq_s==freq_t:
+            return True
+        return False
+
         
         
         
