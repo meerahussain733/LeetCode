95b871aa62459a1cd32745b950403223072bba40
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2025-12-25 19:18:32 -0500

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

---
+++ hash-table/roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        h = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+        val=0
+        for char in range(len(s)-1):
+            if h[s[char]]>=h[s[char+1]]:
+                val=val+h[s[char]]
+                char+=1
+            else:
+                val=val-h[s[char]]
+                char+=1
+            if char==len(s)-1:
+                val=val+h[s[char]]
+                return val
+
+    
+        
+
+        
