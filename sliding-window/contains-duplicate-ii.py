af10468d0c1c7521291e328877b92868b105a321
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-04 19:59:27 -0500

    LC-contains-duplicate-ii [Runtime: N/A; Memory: N/A]

--- sliding-window/contains-duplicate-ii.py
+++ sliding-window/contains-duplicate-ii.py
@@ -1,9 +1,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         #brute force
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
+        for i in range(len(nums)-k):
+            for j in range(i+1,k+i+1):
                 if nums[i]==nums[j] and abs(i-j)<=k:
                     return True 
+            i+=k
         return False
         
