e2370541fddf0851115e636a4627ac3334465d0e
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-05 21:55:49 -0500

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

--- array/top-k-frequent-elements.py
+++ array/top-k-frequent-elements.py
@@ -1,15 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq_ele={}
-        top_k=[]
         for element in nums:
             if element in freq_ele:
                 freq_ele[element]+=1
             else:
                 freq_ele[element]=1
         sorted_frq=sorted(freq_ele)
-        for i in range(k):
-            top_k.append(sorted_frq[i])
-        return top_k
-
+        return sorted_frq[:k]
         
