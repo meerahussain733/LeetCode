61c3348330fecdaf0d3903c8f97453bfcd0ac6a1
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-05 21:46:52 -0500

    LC-contains-duplicate-ii [Runtime: 31 ms; Memory: 27.9 MB]

--- hash-table/contains-duplicate-ii.py
+++ hash-table/contains-duplicate-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        #brute force
-        for i in range(len(nums)-k):
-            for j in range(i+1,k+i+1):
-                if nums[i]==nums[j] and abs(i-j)<=k:
-                    return True 
-            i+=k-1
-        return False
+        window=set()
+        for index, number in enumerate(nums):
+            if number in window:
+                return True
+            window.add(number)
+
+            if index >= k:
+                window.remove(nums[index-k])
         
+        return False
+    
