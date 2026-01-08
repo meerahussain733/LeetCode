7fe73fd13c1a059058de29d2a4785eac438a2b1d
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-08 12:53:03 -0500

    LC-find-most-frequent-vowel-and-consonant [Runtime: 3 ms; Memory: 19.4 MB]

---
+++ hash-table/find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        vowel_map={'a':0,'e':0,'i':0,'o':0,'u':0}
+        const_map={}
+        for char in s:
+            if char in vowel_map:
+                vowel_map[char]+=1
+                continue
+            if char in const_map:
+                const_map[char]+=1
+            else:
+                const_map[char]=1
+        
+        Sum=max(vowel_map.values(),default=0)+max(const_map.values(),default=0)
+        return Sum
+        
