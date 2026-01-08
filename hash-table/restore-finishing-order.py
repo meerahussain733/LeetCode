5a6b85d6b5aeff5181493f68b7412c82bf7be639
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-07 23:21:23 -0500

    LC-restore-finishing-order [Runtime: 0 ms; Memory: 19.4 MB]

--- hash-table/restore-finishing-order.py
+++ hash-table/restore-finishing-order.py
@@ -1,10 +1,8 @@
 class Solution:
     def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
         hash_map={}
-        for i in range(len(friends)):
-            for j in range(len(order)):
-                if friends[i]==order[j]:
-                    hash_map[friends[i]]=j
+        for i in range(len(order)):
+            hash_map[order[i]]=i
         return sorted(friends, key=lambda x: hash_map[x])
 
         
