b55d05da3e212f18b61b5334effb90f34c7ba7ea
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-07 23:40:54 -0500

    LC-number-of-good-pairs [Runtime: 0 ms; Memory: 19.4 MB]

--- hash-table/number-of-good-pairs.py
+++ hash-table/number-of-good-pairs.py
@@ -1,9 +1,15 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        count=0
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]==nums[j]:
-                    count+=1
-        return count
+        hash_map={}
+        for i in nums:
+            if i in hash_map:
+                hash_map[i]+=1
+            else:
+                hash_map[i]=1
+
+        pairs=0
+        for freq in hash_map.values():
+            pairs+=freq*(freq-1)//2
+        return pairs
+        
         
