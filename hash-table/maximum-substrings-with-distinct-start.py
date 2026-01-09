5e416194c1e2d55ee8c3764bfd703880e57f9593
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-09 18:25:18 -0500

    LC-maximum-substrings-with-distinct-start [Runtime: 7 ms; Memory: 20.1 MB]

--- hash-table/maximum-substrings-with-distinct-start.py
+++ hash-table/maximum-substrings-with-distinct-start.py
@@ -1,12 +1,7 @@
 class Solution:
     def maxDistinct(self, s: str) -> int:
-        count=0
-        hash_set=set()
-        for i in s:
-            if i not in hash_set:
-                hash_set.add(i)
-                count+=1
-        return count
+        hash_set=set(s)
+        return len(hash_set)
                 
 
 
