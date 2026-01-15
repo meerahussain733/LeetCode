ca786b834ccaea5f7b37f5874fa2f71b25c08f64
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-14 23:35:17 -0500

    LC-group-anagrams [Runtime: 11 ms; Memory: 22.3 MB]

---
+++ string/group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=defaultdict(list)
+        for str in strs:
+            sort_str=''.join(sorted(str))
+            res[sort_str].append(str)
+        return list(res.values())
+
+        
