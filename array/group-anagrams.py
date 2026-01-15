2b90d3523a5bff6d8317ad8206693257450827ac
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-14 23:52:30 -0500

    LC-group-anagrams [Runtime: 17 ms; Memory: 24 MB]

--- array/group-anagrams.py
+++ array/group-anagrams.py
@@ -1,9 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res=defaultdict(list)
-        for str in strs:
-            sort_str=''.join(sorted(str))
-            res[sort_str].append(str)
+        for s in strs:
+            count=[0]*26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            res[tuple(count)].append(s)
         return list(res.values())
 
         
