8af7faf56ba0a98612ad771236824af708e71bd6
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-18 20:59:00 -0500

    LC-longest-common-subsequence [Runtime: N/A; Memory: N/A]

--- string/longest-common-subsequence.py
+++ string/longest-common-subsequence.py
@@ -1,8 +1,9 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         hash_set=set(text1)
+        hash_set2=set(text2)
         count=0
-        for i in text2:
+        for i in hash_set2:
             if i in hash_set:
                 count+=1
         return count
