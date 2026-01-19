bf37660ecf08dc60195f8885f7528027a7328ffc
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-19 17:27:45 -0500

    LC-longest-consecutive-sequence [Runtime: 41 ms; Memory: 36.7 MB]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -16,4 +16,5 @@ class Solution:
                 max_count=max(max_count,count)
             else:
                 count=1
+                max_count=max(max_count,count)
         return max_count
