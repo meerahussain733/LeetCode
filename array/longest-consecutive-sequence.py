2eb3ac6e6f6b6f7aeee627badf7f957656c05e17
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-19 17:26:49 -0500

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -3,10 +3,17 @@ class Solution:
         num_set=set(nums)
         if not nums:
             return 0
+        
+        max_count=0
         count=1
+        
         sort_num=sorted(num_set)
+        if len(sort_num)==1:
+            return 1
         for i in range(len(sort_num)-1):
             if sort_num[i]+1 ==sort_num[i+1]:
                 count+=1
-        return count
-        
+                max_count=max(max_count,count)
+            else:
+                count=1
+        return max_count
