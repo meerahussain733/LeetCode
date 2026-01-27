66f2e03868d57f2c6835e22d222bc9261bc2a910
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-26 21:44:57 -0500

    LC-search-a-2d-matrix [Runtime: 0 ms; Memory: 19.5 MB]

--- matrix/search-a-2d-matrix.py
+++ matrix/search-a-2d-matrix.py
@@ -2,9 +2,16 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m=len(matrix)
         n=len(matrix[0])
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j]==target:
-                    return True 
+        low=0
+        high=m*n-1
+        while low<=high:
+            mid=(low+high)//2
+            row,col=mid//n,mid%n
+            if matrix[row][col]==target:
+                return True
+            elif matrix[row][col]<target:
+                low=mid+1
+            else:
+                high=mid-1
         return False
         
