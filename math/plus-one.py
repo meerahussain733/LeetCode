f66fe51f0a661c70bf80a24adfb6832aa8973e44
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-30 00:07:37 -0500

    LC-plus-one [Runtime: 0 ms; Memory: 19.5 MB]

--- math/plus-one.py
+++ math/plus-one.py
@@ -1,6 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        new_digits=digits
-        new_digits[-1]+=1
+        new_digits = int("".join(map(str, digits))) +1
+        new_digits = [int(digit) for digit in str(new_digits)]
         return new_digits
         
