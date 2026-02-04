9d396312c461a2f7f573423cc0b29961d9768e39
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-02-03 21:40:29 -0500

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -2,5 +2,5 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n==1:
             return 1
-        return (n*(self.climbStairs(n-1)))//(n-1)
+        return int((n*(self.climbStairs(n-1)))/(n-1))
         
