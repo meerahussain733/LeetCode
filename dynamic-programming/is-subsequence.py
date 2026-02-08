e0f159041f6ca9be1084c83802022d85d6d17ec6
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-02-08 17:23:12 -0500

    LC-is-subsequence [Runtime: N/A; Memory: N/A]

--- dynamic-programming/is-subsequence.py
+++ dynamic-programming/is-subsequence.py
@@ -1,10 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        c=0
-        for i in s:
-            if i in t:
-                c+=1
-        if c==len(s):
-            return True 
+        count=0
+        for i in range(len(s)):
+            for j in range(i,len(t)):
+                if s[i]==t[j]:
+                    count+=1
+                    j+=1
+        if count==len(s):
+            return True
         return False
+        
         
