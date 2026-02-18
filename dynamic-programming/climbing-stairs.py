39b6b41417ffeb37b77131e47ae22c9162a98a83
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-02-17 21:17:01 -0500

    LC-climbing-stairs [Runtime: 0 ms; Memory: 19.5 MB]

--- dynamic-programming/climbing-stairs.py
+++ dynamic-programming/climbing-stairs.py
@@ -1,6 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==1:
-            return 1
-        return int((n*(self.climbStairs(n-1)))/(n-1))
-        
+        ways = 1
+
+        for i in range(1, (n // 2) + 1):
+            product = 1
+
+            for j in range(i, 2 * i):
+                product *= (n - j) / (j - i + 1)
+
+            ways += product
+
+        return int(ways)
+
