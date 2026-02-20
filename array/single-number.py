09bc56de00b9b7cc06fafd15688fa3640cfa51fd
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-02-19 22:39:23 -0500

    LC-single-number [Runtime: 4 ms; Memory: 21.8 MB]

---
+++ array/single-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_map={}
+        for i in nums:
+            if i in hash_map:
+                hash_map[i]+=1
+            else:
+                hash_map[i]=1
+        for key, value in hash_map.items():
+            if value == 1:
+                return key
+        
